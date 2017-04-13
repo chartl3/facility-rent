003115781963028adb399902a9c319893ad6fe20
EC2 Default User <ec2-user@ip-172-31-35-166.us-west-2.compute.internal>
2017-04-13 23:30:40 +0000

    Add index file

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
-<?php echo '<h1>
-<img src="http://img12.funscrape.com/en/FunnyHello/4.gif";
- height="500" width="600"/>
-
+<html>
+ <head>
+  <title> Honedu </title>
+ </head>
+ <body>
+ <h1> Gihad is a terrible programmer  </h1>
+ </body>
 </html>
-</h1>'; ?>
-
