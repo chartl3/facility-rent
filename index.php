b103b08cbada93680c825c9050f7a6214c1907a4
root <root@ip-172-31-35-166.us-west-2.compute.internal>
2017-04-14 00:02:10 +0000

    Add Index

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <html>
- <head>
-  <title> Honedu </title>
- </head>
- <body>
- <h1> Gihad is a terrible programmer  </h1>
- </body>
+<?php
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+?>
 </html>
